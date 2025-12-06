72067074c7061f92a4db3b264da584796f123f58
play-aviator <rehema191919@gmail.com>
2025-12-06 03:14:26 +0300

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,1 +1,29 @@
+export default {
+  async fetch(request) {
+    // Rarely below 2.5x
+    // Up to 10,000x occasionally
+    function generateMultiplier() {
+      const r = Math.random();
+
+      if (r < 0.02) {
+        return (Math.random() * 9997 + 3).toFixed(2);  
+      }
+
+      if (r < 0.30) {
+        return (Math.random() * 8 + 2.5).toFixed(2);
+      }
+
+      return (Math.random() * 4 + 2.5).toFixed(2);
+    }
+
+    const result = {
+      multiplier: generateMultiplier(),
+      time: Date.now()
+    };
+
+    return new Response(JSON.stringify(result), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+};
 
